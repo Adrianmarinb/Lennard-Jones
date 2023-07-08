ce2974f7dbd92beb6bb6b5a040aca5094dfb5d29
Adrianmarinb <e.marinadrian@go.ugr.es>
2023-07-08 08:11:13 +0200

    Scripts de Lennard-Jones

---
+++ Compu/Potencial de Lennard-Jones/parameters_only_one.py
@@ -0,0 +1,403 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 12 18:42:06 2023
+
+@author: adria
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+from mpl_toolkits.axes_grid1.inset_locator import mark_inset, inset_axes
+import os
+print(os.getcwd())
+
+#-------------------------------------------------------------
+#We extract omega from .txt and remove points with no values
+
+
+with open('parameters.txt') as f:
+    lines = f.readlines()[0:]
+
+t = []
+potential = []
+kinetic = []
+total = []
+velocity = []
+momentum = []
+fluctuations = []
+distance_1p = []
+distance_2p = []
+distance_3p = []
+
+for line in lines:
+    values = line.strip().split(' ')
+    t.append(float(values[0]))
+    potential.append(float(values[1]))
+    kinetic.append(float(values[2]))
+    total.append(float(values[3]))
+    velocity.append(float(values[4]))
+    momentum.append(float(values[5]))
+    fluctuations.append(float(values[6]))
+    distance_1p.append(float(values[7]))
+    distance_2p.append(float(values[8]))
+    distance_3p.append(float(values[9]))
+    
+std_dev = np.std(total)
+mean_e = np.mean(total)
+print('Mean value of total energy: ', mean_e)
+print('Standar deviation of total energy: ', std_dev)
+
+#-------------------------------------------------------------
+# Calculate the average value of velocity during a period of time
+start_time = 5  # Specify the start time of the period
+end_time = 30    # Specify the end time of the period
+
+time_indices = np.where((np.array(t) >= start_time) & (np.array(t) <= end_time))
+velocity_period = np.array(velocity)[time_indices]
+average_velocity = np.mean(velocity_period)
+
+# Print the average value
+print("Average sum of square velocities during the period from", start_time, "to", end_time, "is:", round(average_velocity, 2), 'm2/s2')
+Temperature = 0.5*average_velocity
+print("The temperature of the system is", round(Temperature, 2), "K")
+
+#-------------------------------------------------------------
+# Comparison between temperatures
+
+with open('comparison.tsv') as f:
+    lines = f.readlines()[0:]
+
+times = []
+T1 = []
+T2 = []
+T3 = []
+T4 = []
+
+for line in lines:
+    values = line.strip().split('\t')
+    times.append(float(values[0]))
+    T1.append(float(values[1]))
+    T2.append(float(values[2]))
+    T3.append(float(values[3]))
+    T4.append(float(values[4]))
+
+#-------------------------------------------------------------
+# Plot evolution of temperature for each 0.5 second
+
+start_time = 5  # Specify the start time of the period
+end_time = 30    # Specify the end time of the period
+
+# Assuming you have arrays t and velocity defined
+
+interval = 1  # Specify the interval for calculating average velocity
+time_indices_t = np.arange(start_time, end_time, interval)
+
+average_velocities = []
+
+for time in time_indices_t:
+    time_range = np.where((np.array(t) >= time) & (np.array(t) < time + interval))
+    velocity_period = np.array(velocity)[time_range]
+    average_velocity = np.mean(velocity_period)
+    average_velocities.append(average_velocity)
+
+average_velocities = np.array(average_velocities)
+temperatures = 0.5 * average_velocities
+temp2 = temperatures[:-10]
+
+stdt = np.std(temp2)
+mean = np.mean(temp2)
+print('Mean value of temperature: ', stdt)
+print('Standar deviation of temperature: ', mean)
+
+#-------------------------------------------------------------
+# Study of the pressure of the system
+
+def sum_array_values(arr):
+    total = 0
+    for value in arr:
+        total += value
+    return total
+
+momentum_period = np.array(momentum)[time_indices]
+Force = (1/(end_time - start_time)) * sum_array_values(momentum_period)
+
+Area = 40
+
+Pressure = Force / Area
+
+Relation = Pressure / Temperature
+
+print("The relationship Pressure/Temperature of the system is", round(Relation, 3), "K. Giving us a pressure of:", round(Pressure, 3), "Pa.")
+print("With the Ideal gases equation, the pressure should be", round(Temperature * 16 / 100, 3), "units. This relationship would be", round(16 / 100, 3 ))
+
+#-------------------------------------------------------------
+# Study of velocities
+
+v_distribution = []
+v_module = []
+vx = []
+vy = []
+
+v_new = np.linspace(0, 10, num=1000)
+vx_new = np.linspace(-7, 7, num=1000)
+
+def maxwell_distribution(v, T):
+    exponent = -v**2 / (2 * T)
+    return (v/T) * np.exp(exponent)
+
+def gaussian_distribution(v, T):
+    exponent = -v**2 / (2 * T)
+    return np.sqrt(1/(2*np.pi*T)) * np.exp(exponent)
+
+with open('v_statistics.txt') as f:
+    lines = f.readlines()[0:]
+  
+for line in lines:
+    values = line.strip().split(' ')
+    v_distribution.append(float(values[0]))
+    v_module.append(float(values[1]))
+    vx.append(float(values[2]))
+    vy.append(float(values[3]))
+    
+PDF = maxwell_distribution(np.array(v_new), Temperature)
+PDF_vx = gaussian_distribution(np.array(vx_new), Temperature)
+PDF_vy = gaussian_distribution(np.array(vx_new), Temperature)
+    
+#-------------------------------------------------------------
+# Study of g_function
+
+g_dist = []
+g_values = []
+
+with open('g_statistics.txt') as f:
+    lines = f.readlines()[0:]
+  
+for line in lines:
+    values = line.strip().split(' ')
+    g_dist.append(float(values[0]))
+    g_values.append(float(values[1]))
+
+#-------------------------------------------------------------
+# Plotting the data
+    
+#fig, ax = plt.subplots(1, 1, figsize=(20,8)) #Para energias y temperatura y maxwellV0
+#fig, ax = plt.subplots(1, 1, figsize=(10,10))
+fig, ax = plt.subplots(1, 1, figsize=(20,10))
+
+plt.style.use('default')
+
+plt.rc('xtick', labelsize=25)
+plt.rc('ytick', labelsize=25)
+
+gs = gridspec.GridSpec(1, 1)
+scatter_legend_size = 60
+
+
+#---Subplot 1------------
+ax1 = plt.subplot(gs[0, 0])
+ax1.tick_params(axis='x', direction='out', pad=15)
+ax1.tick_params(axis='y', direction='out', pad=15)
+plt.scatter(t, potential, s=5, label='Energía potencial media')
+plt.scatter(t, kinetic, 5, label='Energía cinética media')
+plt.scatter(t, total, 5, label='Energía total media')
+#plt.plot(time_indices_t, 0.5*average_velocities, linewidth = 5, label='Evolución de T para intervalos de 1 s', color='orange')
+plt.xlabel('t (s)', fontsize=25, labelpad=20)
+plt.ylabel('E/ϵ', fontsize=25, labelpad=15)
+#plt.title('Potencial de Lennard-Jones', fontsize=15, pad=10)  
+
+plt.axvline(x=20, color='black', linestyle='--', lw=2, label= "Reescalado de velocidad (1.1)", alpha=0.3)
+plt.axvline(x=25, color='black', linestyle='--', lw=2, alpha=0.3)
+plt.axvline(x=30, color='black', linestyle='--', lw=2, alpha=0.3)
+plt.axvline(x=35, color='black', linestyle='--', lw=2, alpha=0.3)
+plt.axvline(x=40, color='black', linestyle='--', lw=2, alpha=0.3)
+plt.axvline(x=45, color='black', linestyle='--', lw=2, alpha=0.3)
+plt.axvline(x=50, color='black', linestyle='--', lw=2, alpha=0.3)
+plt.axvline(x=55, color='black', linestyle='--', lw=2, alpha=0.3)
+plt.axvline(x=60, color='black', linestyle='--', lw=2, alpha=0.3)
+plt.axvline(x=65, color='black', linestyle='--', lw=2, alpha=0.3)
+plt.axvline(x=70, color='black', linestyle='--', lw=2, alpha=0.3)
+plt.axvline(x=75, color='black', linestyle='--', lw=2, alpha=0.3)
+plt.axvline(x=80, color='black', linestyle='--', lw=2, alpha=0.3)
+plt.axvline(x=85, color='black', linestyle='--', lw=2, alpha=0.3)
+plt.axvline(x=90, color='black', linestyle='--', lw=2, alpha=0.3)
+plt.axvline(x=95, color='black', linestyle='--', lw=2, alpha=0.3)
+plt.axvline(x=100, color='black', linestyle='--', lw=2, alpha=0.3)
+
+legend = ax1.legend(loc=9, prop={'size': 25})
+for legend_handle in legend.legendHandles:
+    legend_handle._sizes = [scatter_legend_size]
+#ax1.set_xlim(-1, 11)
+#ax1.set_ylim(-2.5,4 )
+plt.xticks(np.arange(-0, 100.01, 5))
+plt.grid()
+
+axins = inset_axes(ax1, 2.8, 2.8, loc=1, bbox_to_anchor=(0.32, 0.9), bbox_transform=ax1.figure.transFigure)
+
+axins.set_xlim(2, 6)
+axins.set_ylim(-2.25, 1.35)
+plt.xticks(visible=False)
+plt.yticks(visible=False)
+mark_inset(ax1, axins, loc1=2, loc2=4, fc="none", ec="0.5")
+axins.scatter(t, potential, 5)
+axins.scatter(t, kinetic, 5)
+axins.scatter(t, total, 5)
+
+'''
+ax1 = plt.subplot(gs[0, 0])
+#plt.hist(v_hist, bins= 5, range=(0, 2), label='|V|', color='green', linewidth=2, edgecolor = "white", width=0.2)
+plt.bar(v_distribution, v_module, label='Distribución de velocidades registradas en t = 0.000 s', color='green', linewidth=2, edgecolor = "white", width=0.2)
+plt.scatter(v_new, PDF, label='Distribución teórica de Maxwell para T', color='red')
+plt.xlabel('v (r/σs)', fontsize=25, labelpad=20)
+plt.ylabel('P(v)', fontsize=25, labelpad=20)
+legend = ax1.legend(loc=1, prop={'size': 25})
+for legend_handle in legend.legendHandles:
+    legend_handle._sizes = [scatter_legend_size]
+ax1.set_xlim(-0.2, 1.5)
+#ax1.set_ylim(0, 0.16)
+plt.xticks(np.arange(-0.2, 8, 0.5))
+plt.grid()
+'''
+'''
+ax1 = plt.subplot(gs[0, 0])
+plt.grid()
+#plt.hist(v_hist, bins= 5, range=(0, 2), label='|V|', color='green', linewidth=2, edgecolor = "white", width=0.2)
+plt.bar(v_distribution, vx, label='Distribución registrada para v$_x$', linewidth=1, edgecolor = "white", width=0.2)
+plt.scatter(vx_new, PDF_vx, label='Distribución teórica Gaussiana para T', color='red')
+plt.xlabel('v$_x$ (r/σs)', fontsize=25, labelpad=20)
+plt.ylabel('P(v$_x$)', fontsize=25, labelpad=20)
+legend = ax1.legend(loc=1, prop={'size': 25})
+for legend_handle in legend.legendHandles:
+    legend_handle._sizes = [scatter_legend_size]
+ax1.set_xlim(-4, 4)
+#ax1.set_ylim(0, 26)
+plt.xticks(np.arange(-7, 7, 1))
+'''
+'''
+ax1 = plt.subplot(gs[0, 0])
+plt.grid()
+#plt.hist(v_hist, bins= 5, range=(0, 2), label='|V|', color='green', linewidth=2, edgecolor = "white", width=0.2)
+plt.bar(v_distribution, vy, label='Distribución registrada para v$_y$', linewidth=1, edgecolor = "white", width=0.2, color='orange')
+plt.scatter(vx_new, PDF_vy, label='Distribución teórica Gaussiana para T', color='red')
+plt.xlabel('v$_y$ (r/σs)', fontsize=25, labelpad=20)
+plt.ylabel('P(v$_y$)', fontsize=25, labelpad=20)
+legend = ax1.legend(loc=1, prop={'size': 25})
+for legend_handle in legend.legendHandles:
+    legend_handle._sizes = [scatter_legend_size]
+ax1.set_xlim(-4, 4)
+#ax1.set_ylim(0, 26)
+plt.xticks(np.arange(-7, 7, 1))
+'''
+'''
+ax1 = plt.subplot(gs[0, 0])
+ax1.tick_params(axis='x', direction='out', pad=15)
+ax1.tick_params(axis='y', direction='out', pad=15)
+plt.plot(times, T1, color='orange', linewidth=5, label='T para v = 1')
+plt.plot(times, T2, times, T3, times, T4, linewidth=5)
+plt.xlabel('t (s)', fontsize=25, labelpad=20)
+plt.ylabel('E/ϵ', fontsize=25, labelpad=15)
+legend = ax1.legend(['T para v = 1', 'T para v = 2', 'T para v = 3', 'T para v = 4'], loc=1, prop={'size': 25})
+ax1.set_ylim(8, 18)
+plt.grid()
+'''
+
+'''
+ax1 = plt.subplot(gs[0, 0])
+plt.scatter(t, fluctuations, 2, label='Fluctuación para la partícula 1', color='purple')
+#plt.scatter(t, distance_2p, 2, label='Distance between 2 particles', color='purple')
+plt.plot(time_indices_t, 0.5*average_velocities, linewidth = 3, label='Evolución de la temperatura T (K)', color='orange')
+plt.xlabel('t (s)', fontsize=25, labelpad=20)
+plt.ylabel('$<(r(t) - r(t=0))^2> (\sigma) $  ', fontsize=25, labelpad=20)
+
+plt.axvline(x=20, color='black', linestyle='--', lw=2, label= 'Reescalado de velocidad (1.5)')
+plt.axvline(x=30, color='black', linestyle='--', lw=2)
+plt.axvline(x=35, color='black', linestyle='--', lw=2)
+plt.axvline(x=45, color='black', linestyle='--', lw=2)
+
+legend = ax1.legend(loc=0, prop={'size': 25})
+for legend_handle in legend.legendHandles:
+    legend_handle._sizes = [scatter_legend_size]
+    
+plt.xticks(np.arange(-0, 50.01, 5))
+    
+plt.grid()
+'''
+'''
+interval_r = 0.5  # Specify the interval for calculating average velocity
+time_indices_r = np.arange(start_time, end_time, interval_r)
+
+r1 = []
+r2 = []
+r3 = []
+
+
+for time in time_indices_r:
+    time_range = np.where((np.array(t) >= time) & (np.array(t) < time + interval))
+    period_d1 = np.array(distance_1p)[time_range]
+    r1_t = np.mean(period_d1)
+    r1.append(r1_t)
+    
+    period_d2 = np.array(distance_2p)[time_range]
+    r2_t = np.mean(period_d2)
+    r2.append(r2_t)
+    
+    period_d3 = np.array(distance_3p)[time_range]
+    r3_t = np.mean(period_d3)
+    r3.append(r3_t)
+
+
+
+ax1 = plt.subplot(gs[0, 0])
+#plt.plot(time_indices_r, r1, time_indices_r, r2, time_indices_r, r3, linewidth=5)
+plt.plot(time_indices_r, r1, color='red', linewidth=5)
+plt.plot(time_indices_r, r2, color='purple', linewidth=5)
+plt.plot(time_indices_r, r3, color='pink', linewidth=5)
+plt.plot(time_indices_t, 0.5*average_velocities, color='orange', linewidth=5)
+plt.xlabel('t (s)', fontsize=25, labelpad=20)
+plt.ylabel('<$(\Delta r_{ij}(t))^2> (\sigma)$', fontsize=25, labelpad=20)
+plt.legend(loc=0, prop={'size': 25})
+
+plt.axvline(x=20, color='black', linestyle='--', lw=2, label= "Reescalado de velocidad (1.1)", alpha=0.3)
+plt.axvline(x=25, color='black', linestyle='--', lw=2, alpha=0.3)
+plt.axvline(x=30, color='black', linestyle='--', lw=2, alpha=0.3)
+plt.axvline(x=35, color='black', linestyle='--', lw=2, alpha=0.3)
+plt.axvline(x=40, color='black', linestyle='--', lw=2, alpha=0.3)
+plt.axvline(x=45, color='black', linestyle='--', lw=2, alpha=0.3)
+plt.axvline(x=50, color='black', linestyle='--', lw=2, alpha=0.3)
+plt.axvline(x=55, color='black', linestyle='--', lw=2, alpha=0.3)
+plt.axvline(x=60, color='black', linestyle='--', lw=2, alpha=0.3)
+plt.axvline(x=65, color='black', linestyle='--', lw=2, alpha=0.3)
+plt.axvline(x=70, color='black', linestyle='--', lw=2, alpha=0.3)
+plt.axvline(x=75, color='black', linestyle='--', lw=2, alpha=0.3)
+plt.axvline(x=80, color='black', linestyle='--', lw=2, alpha=0.3)
+plt.axvline(x=85, color='black', linestyle='--', lw=2, alpha=0.3)
+plt.axvline(x=90, color='black', linestyle='--', lw=2, alpha=0.3)
+plt.axvline(x=95, color='black', linestyle='--', lw=2, alpha=0.3)
+plt.axvline(x=100, color='black', linestyle='--', lw=2, alpha=0.3)
+
+legend = ax1.legend(['Separación cuadrática entre p1 y p2', 'Separación cuadrática entre p1 y p6', 'Separación cuadrática entre p1 y p8', 'Evolución de la temperatura T (K)'], loc=0, prop={'size': 25})
+for legend_handle in legend.legendHandles:
+    legend_handle._sizes = [scatter_legend_size]
+    
+plt.xticks(np.arange(-0, 100.01, 5))
+
+plt.grid()
+'''
+'''
+#---Subplot 6------------
+ax5 = plt.subplot(gs[0, 0])
+plt.bar(g_dist, g_values, label='Función de correlación de pares para p1 en estado gaseoso', color='brown', linewidth=1, edgecolor = "white", width=0.042)
+plt.xlabel('r ($\sigma$)', fontsize=25, labelpad=20)
+plt.ylabel('g(r)', fontsize=25, labelpad=20)
+plt.legend(loc=0, prop={'size': 20})
+plt.xticks(np.arange(1, 3.5, 0.25))
+plt.grid()
+'''
+
+
+
+
+
+
+
+
+
---
+++ Compu/Potencial de Lennard-Jones/show particles time.py
@@ -0,0 +1,239 @@
+# -*- coding: utf-8 -*-
+# ================================================================================
+# ANIMACION SISTEMA SOLAR
+#
+# Genera una animación a partir de un fichero de datos con las posiciones
+# de los planetas en diferentes instantes de tiempo.
+# 
+# El fichero debe estructurarse de la siguiente forma:
+# 
+#   t_1
+#   x1_1, y1_1
+#   x2_1, y2_1
+#   x3_1, y3_1
+#   (...)
+#   xN_1, yN_1
+#   
+#   t_2
+#   x1_2, y1_2
+#   x2_2, y2_2
+#   x3_2, y3_2
+#   (...)
+#   xN_2, yN_2
+#
+#   t_3
+#   x1_3, y1_3
+#   x2_3, y2_3
+#   x3_3, y3_3
+#   (...)
+#   xN_3, yN_3
+#   
+#   (...)
+#
+# donde xi_j es la componente x del planeta i-ésimo en el instante de
+# tiempo j-ésimo, e yi_j lo mismo en la componente y. El programa asume que
+# el nº de planetas es siempre el mismo.
+# ¡OJO! Los datos están separados por comas.
+# 
+# Si solo se especifica un instante de tiempo, se genera una imagen en pdf
+# en lugar de una animación
+#
+# Se puede configurar la animación cambiando el valor de las variables
+# de la sección "Parámetros"
+#
+# ================================================================================
+
+# Importa los módulos necesarios
+from matplotlib import pyplot as plt
+from matplotlib.animation import FuncAnimation
+from matplotlib.patches import Circle, Rectangle
+import numpy as np
+from matplotlib import cm
+
+# Parámetros
+# ========================================
+file_in = "lennard-jones-output.txt"  # Nombre del fichero de datos
+file_out = "particles"  # Nombre del fichero de salida (sin extensión)
+
+
+# Límites de los ejes X e Y
+x_min = -1
+x_max = 5
+y_min = -1
+y_max = 6
+
+interval = 20  # Tiempo entre fotogramas en milisegundos
+show_trail = False  # Muestra la "estela" del planeta
+trail_width = 1  # Ancho de la estela
+save_to_file = False  # False: muestra la animación por pantalla,
+# True: la guarda en un fichero
+dpi = 150  # Calidad del vídeo de salida (dots per inch)
+
+# Radio del planeta, en las mismas unidades que la posición
+# Puede ser un número (el radio de todos los planetas) o una lista con
+# el radio de cada uno
+planet_radius = 0.1
+# planet_radius = [0.5, 0.7, 1.1]
+
+# Lectura del fichero de datos
+# ========================================
+# Lee el fichero a una cadena de texto
+with open(file_in, "r") as f:
+    data_str = f.read()
+
+# Inicializa la lista con los datos de cada fotograma.
+# frames_data[j] contiene los datos del fotograma j-ésimo
+frames_data = list()
+
+# Itera sobre los bloques de texto separados por líneas vacías
+# (cada bloque corresponde a un instante de tiempo)
+for frame_data_str in data_str.split("\n\n"):
+    # Inicializa la lista con la posición de cada planeta
+    frame_data = list()
+
+    # Divide las líneas del bloque en dos partes:
+    # - La primera línea es el tiempo (t)
+    # - Las siguientes líneas son las posiciones de los planetas
+    lines = frame_data_str.strip().split("\n")
+    t_line = lines[0].strip()  # Get the first line (time line)
+    if t_line:
+        t = float(t_line)  # Convert the time value to float
+        frame_data.append(t)  # Add the time to the frame data
+
+        # Iterate over the remaining lines (planet positions)
+        for planet_pos_str in lines[1:]:
+            planet_pos = np.fromstring(planet_pos_str, sep=",")
+            # If the line is not empty, add the planet position to the frame data
+            if planet_pos.size > 0:
+                frame_data.append(np.fromstring(planet_pos_str, sep=","))
+
+        # Add the data of this frame to the list
+        frames_data.append(frame_data)
+
+# El número de planetas es el número de líneas en cada bloque - 1
+# Lo calculamos del primer bloque
+nplanets = len(frames_data[0]) - 1
+
+# Generate a set of colors using the viridis color map
+colors = cm.viridis(np.linspace(0, 1, nplanets))
+
+# Creación de la animación/gráfico
+# ========================================
+# Crea los objetos figure y axis
+fig, ax = plt.subplots(figsize=(6, 6.54))
+
+# Define el rango de los ejes
+ax.axis("equal")  # Misma escala para ejes X e Y
+ax.set_xlim(x_min, x_max)
+ax.set_ylim(y_min, y_max)
+
+# Set x_label and y_label
+plt.xlabel('x(σ)', fontsize = 15)
+plt.ylabel('y(σ)', fontsize = 15)
+
+ax.tick_params(axis="both", which="major", labelsize=15, pad=10)
+
+box = Rectangle((0, 0), 4, 4, edgecolor='green', linewidth=4, fill=False)
+ax.add_patch(box)
+
+# Si solo se ha dado un radio para todos los planetas, conviértelo a una
+# lista con todos los elementos iguales
+if not hasattr(planet_radius, "__iter__"):
+    planet_radius = planet_radius * np.ones(nplanets)
+# En caso contrario, comprueba que el nº de radios coincide con el
+# nº de planetas y devuelve error en caso contrario
+else:
+    if not nplanets == len(planet_radius):
+        raise ValueError(
+            "El número de radios especificados no coincide con el número "
+            "de planetas")
+
+# Representa el primer fotograma
+# Pinta un punto en la posición de cada paneta y guarda el objeto asociado
+# al punto en una lista
+planet_points = list()
+planet_trails = list()
+for planet_pos, radius, color in zip(frames_data[0][1:], planet_radius, colors):
+    x, y = planet_pos
+    planet_point = Circle((x, y), radius)
+    planet_point.set_facecolor(color)
+    ax.add_artist(planet_point)
+    planet_points.append(planet_point)
+
+    # Inicializa las estelas (si especificado en los parámetros)
+    if show_trail:
+        planet_trail, = ax.plot(
+            x, y, "-", linewidth=trail_width,
+            color=planet_points[-1].get_facecolor())
+        planet_trails.append(planet_trail)
+
+# Create the text element for displaying the time value
+time_text = ax.text(0.07, 0.90, '', transform=ax.transAxes, fontsize=15)
+
+# Función que actualiza la posición de los planetas en la animación 
+def update(frame, frames_data, planet_points, planet_trails, show_trail):
+    # Get the time value for the current frame
+    t = frames_data[frame][0]
+
+    # Actualiza la posición del correspondiente a cada planeta
+    for j_planet, planet_pos in enumerate(frames_data[frame][1:]):
+        x, y = planet_pos
+        planet_points[j_planet].center = (x, y)
+
+        if show_trail:
+            xs_old, ys_old = planet_trails[j_planet].get_data()
+            xs_new = np.append(xs_old, x)
+            ys_new = np.append(ys_old, y)
+
+            planet_trails[j_planet].set_data(xs_new, ys_new)
+
+    # Update the time value in the text element
+    time_text.set_text('Tiempo: {:.3f} s'.format(t))
+
+    return planet_points + planet_trails + [time_text]
+
+def init_anim():
+    # Clear trails 
+    if show_trail:
+        for j_planet in range(nplanets):
+            planet_trails[j_planet].set_data(list(), list())
+
+    # Update the time value in the text element
+    t = frames_data[0][0]
+    time_text.set_text('Tiempo: {:.3f} s'.format(t))
+
+    return planet_points + planet_trails + [time_text]
+
+# Calcula el nº de frames
+nframes = len(frames_data)
+
+# Si hay más de un instante de tiempo, genera la animación
+if nframes > 1:
+    # Info sobre FuncAnimation: https://matplotlib.org/stable/api/animation_api.html
+    animation = FuncAnimation(
+        fig, update, init_func=init_anim,
+        fargs=(frames_data, planet_points, planet_trails, show_trail),
+        frames=len(frames_data), blit=True, interval=interval)
+
+    # Muestra por pantalla o guarda según parámetros
+    if save_to_file:
+        animation.save("{}.mp4".format(file_out), dpi=dpi)
+    else:
+        plt.show()
+# En caso contrario, muestra o guarda una imagen
+else:
+    # Muestra por pantalla o guarda según parámetros
+    if save_to_file:
+        fig.savefig("{}.pdf".format(file_out))
+    else:
+        plt.show()
+    
+
+plt.subplots_adjust(
+top=0.88,
+bottom=0.13,
+left=0.145,
+right=0.91,
+hspace=0.2,
+wspace=0.2
+)
